4f09cddf233b7b61346d3b90733c4303010759b8
euhjainy <157803472+euhjainy@users.noreply.github.com>
2025-01-12 19:55:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -77,6 +77,77 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+
+// close3
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('.section-right .colonnes img[data-overlay]');
+    images.forEach(image => {
+            image.addEventListener('click', function() {
+                const overlayId = this.getAttribute('data-overlay'); 
+                const overlay = document.getElementById('overlay-' + overlayId); 
+                
+                if (overlay) {
+                    overlay.style.display = 'flex';  
+                }
+            });
+        });
+    const closeButtons = document.querySelectorAll('.close3');
+        closeButtons.forEach(button => {
+            button.addEventListener('click', function() {
+                const overlay = this.closest('.overlay'); 
+                overlay.style.display = 'none';
+            });
+        });
+    window.addEventListener('click', function(event) {
+        const overlays = document.querySelectorAll('.overlay');
+        overlays.forEach(overlay => {
+            if (event.target === overlay) {
+            overlay.style.display = 'none'; 
+                }
+            });
+        });
+    });
+    
+    document.querySelectorAll('.colonne2 img').forEach(image => {
+        image.addEventListener('click', function(event) {
+            event.stopPropagation(); 
+        });
+    });
+    
+    document.addEventListener("DOMContentLoaded", function() {
+        const images = document.querySelectorAll('.section-right .colonnes_smartphone img[data-overlay]');
+        images.forEach(image => {
+                image.addEventListener('click', function() {
+                    const overlayId = this.getAttribute('data-overlay'); 
+                    const overlay = document.getElementById('overlay-' + overlayId); 
+                    
+                    if (overlay) {
+                        overlay.style.display = 'flex';  
+                    }
+                });
+            });
+        const closeButtons = document.querySelectorAll('.close3');
+            closeButtons.forEach(button => {
+                button.addEventListener('click', function() {
+                    const overlay = this.closest('.overlay'); 
+                    overlay.style.display = 'none';
+                });
+            });
+        window.addEventListener('click', function(event) {
+            const overlays = document.querySelectorAll('.overlay');
+            overlays.forEach(overlay => {
+                if (event.target === overlay) {
+                overlay.style.display = 'none'; 
+                    }
+                });
+            });
+        });
+
+
+// xlose4
+
+
+
 // slick js pour defilement auto image
 $(document).ready(function(){
     $('.your-class').slick({
